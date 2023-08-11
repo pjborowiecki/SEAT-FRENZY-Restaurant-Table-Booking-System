b7a781239204c77d17d05cf6c1a0eef6a21ad0db
P J Borowiecki <piotr.borowiecki@yahoo.com>
2023-08-11 13:53:39 +0200

    Update footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -29,6 +29,42 @@ export function Footer() {
               <span className="font-bold">{siteConfig.name}</span>
             </Link>
           </section>
+          <section
+            id="footer-links"
+            aria-labelledby="footer-links-heading"
+            className="xs:grid-cols-2 grid flex-1 grid-cols-1 gap-10 sm:grid-cols-3"
+          >
+            {siteConfig.footerNav.map((item) => (
+              <div key={item.title} className="space-y-3">
+                <h4 className="text-base font-medium">{item.title}</h4>
+                <ul className="space-y-3">
+                  {item.items.map((link) => (
+                    <li key={link.title}>
+                      <Link
+                        href={link.href}
+                        target={link?.external ? "_blank" : undefined}
+                        rel={link?.external ? "noreferrer" : undefined}
+                        className="text-sm text-muted-foreground transition-colors hover:text-foreground"
+                      >
+                        {link.title}
+                        <span className="sr-only">{link.title}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </section>
+          <section
+            id="newsletter"
+            aria-labelledby="newsletter-heading"
+            className="space-y-3"
+          >
+            <h4 className="text-base font-medium">
+              Subscribe to our newsletter
+            </h4>
+            {/* <SubscribeToNewsletterForm /> */}
+          </section>
         </section>
         <section
           id="footer-bottom"
@@ -38,13 +74,13 @@ export function Footer() {
           <div className="flex-1 text-left text-sm leading-loose text-muted-foreground">
             Built by{" "}
             <a
-              aria-label="Follow @pjborowiecki"
-              href="https://twitter.com/pjborowiecki"
+              aria-label="author's GitHub profile"
+              href="https://github.com/pjborowiecki"
               target="_blank"
               rel="noreferrer"
               className="font-semibold transition-colors hover:text-foreground"
             >
-              Piotr Borowiecki
+              pjborowiecki
             </a>
             .
           </div>
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,10 +1,13 @@
+import type { FooterItem, MainNavItem } from "@/types"
+
 export type SiteConfig = typeof siteConfig
 
 const links = {
-  twitter: "",
-  facebook: "",
-  instagram: "",
-  github: "https://github.com/pjborowiecki",
+  twitter: "https://twitter.com/",
+  facebook: "https://facebook.com/",
+  instagram: "https://instagram.com/",
+  github:
+    "https://github.com/pjborowiecki/https://github.com/pjborowiecki/SeatFrenzy-Restaurant-Table-Booking-System",
   main: "http://localhost:3000",
   ogImage: "http://localhost:3000/opengraph-image.png",
 }
@@ -16,6 +19,81 @@ export const siteConfig = {
   url: links.main,
   ogImage: links.ogImage,
   links,
-  mainNav: [],
-  footerNav: [],
+  mainNav: [
+    {
+      title: "Landing",
+      items: [
+        {
+          title: "Venues",
+          href: "/",
+          description: "All the venues we have to offer",
+          items: [],
+        },
+        {
+          title: "Blog",
+          href: "/blog",
+          description: "Read our latest blog posts",
+          items: [],
+        },
+        {
+          title: "Dashboard",
+          href: "/dashboard/",
+          description: "Manage your bookings and venues",
+          items: [],
+        },
+      ],
+    },
+  ],
+  footerNav: [
+    {
+      title: "Help",
+      items: [
+        {
+          title: "About",
+          href: "/about",
+          external: false,
+        },
+        {
+          title: "Contact",
+          href: "/contact",
+          external: false,
+        },
+        {
+          title: "Terms",
+          href: "/terms",
+          external: false,
+        },
+        {
+          title: "Privacy",
+          href: "/privacy",
+          external: false,
+        },
+      ],
+    },
+    {
+      title: "Social",
+      items: [
+        {
+          title: "Twitter",
+          href: links.twitter,
+          external: true,
+        },
+        {
+          title: "Facebook",
+          href: links.facebook,
+          external: true,
+        },
+        {
+          title: "Instagram",
+          href: links.instagram,
+          external: true,
+        },
+        {
+          title: "GitHub",
+          href: links.github,
+          external: true,
+        },
+      ],
+    },
+  ] satisfies FooterItem[],
 }
